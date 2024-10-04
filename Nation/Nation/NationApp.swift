ac4a038f0629f34cc596015b3567f9b9ed7f5202
andersonbressane <anderbressane@gmail.com>
2024-10-04 09:08:23 -0300

    - Project created;

---
+++ Nation/Nation/NationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NationApp.swift
+//  Nation
+//
+//  Created by Anderson Bressane on 04/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct NationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
